7ec1323a2f4ccd336acc424616f8a827fa30c01a
Zach Ahn <engineering@zachahn.com>
2023-03-21 22:40:11 -0400

    Implement NoTypeAliases

--- lib/rubocop/cop/no_rubocop/no_type_aliases.rb
+++ lib/rubocop/cop/no_rubocop/no_type_aliases.rb
@@ -8,60 +8,23 @@ module RuboCop
       # Examples must have valid Ruby syntax. Do not use upticks.
       #
       # @safety
-      #   Delete this section if the cop is not unsafe (`Safe: false` or
-      #   `SafeAutoCorrect: false`), or use it to explain how the cop is
-      #   unsafe.
+      #   Unsafe
       #
-      # @example EnforcedStyle: bar (default)
-      #   # Description of the `bar` style.
+      # @example
       #
       #   # bad
-      #   bad_bar_method
-      #
-      #   # bad
-      #   bad_bar_method(args)
-      #
-      #   # good
-      #   good_bar_method
-      #
-      #   # good
-      #   good_bar_method(args)
-      #
-      # @example EnforcedStyle: foo
-      #   # Description of the `foo` style.
-      #
-      #   # bad
-      #   bad_foo_method
-      #
-      #   # bad
-      #   bad_foo_method(args)
-      #
-      #   # good
-      #   good_foo_method
-      #
-      #   # good
-      #   good_foo_method(args)
+      #   Foo = T.type_alias { Integer }
       #
       class NoTypeAliases < Base
-        # TODO: Implement the cop in here.
-        #
-        # In many cases, you can use a node matcher for matching node pattern.
-        # See https://github.com/rubocop/rubocop-ast/blob/master/lib/rubocop/ast/node_pattern.rb
-        #
-        # For example
-        MSG = "Use `#good_method` instead of `#bad_method`."
+        MSG = "Do not alias types with `T.type_alias`"
 
-        # TODO: Don't call `on_send` unless the method name is in this list
-        # If you don't need `on_send` in the cop you created, remove it.
-        RESTRICT_ON_SEND = %i[bad_method].freeze
-
-        # @!method bad_method?(node)
-        def_node_matcher :bad_method?, <<~PATTERN
-          (send nil? :bad_method ...)
+        # @!method define_type_alias?(node)
+        def_node_matcher :define_type_alias?, <<~PATTERN
+          (casgn _ _ (block (send (const {nil? | cbase} :T) :type_alias) ...))
         PATTERN
 
-        def on_send(node)
-          return unless bad_method?(node)
+        def on_casgn(node)
+          return unless define_type_alias?(node)
 
           add_offense(node)
         end
--- spec/rubocop/cop/no_rubocop/no_type_aliases_spec.rb
+++ spec/rubocop/cop/no_rubocop/no_type_aliases_spec.rb
@@ -1,21 +1,25 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::NoRubocop::NoTypeAliases, :config do
-  let(:config) { RuboCop::Config.new }
+  let(:message) { described_class::MSG }
 
-  # TODO: Write test code
-  #
-  # For example
-  it "registers an offense when using `#bad_method`" do
+  it "registers an offense when using `T.type_alias`" do
     expect_offense(<<~RUBY)
-      bad_method
-      ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
+      Int = T.type_alias {Integer}
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
     RUBY
   end
 
-  it "does not register an offense when using `#good_method`" do
+  it "registers an offense when using `::T.type_alias`" do
+    expect_offense(<<~RUBY)
+      Int = ::T.type_alias {Integer}
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
+    RUBY
+  end
+
+  it "does not register an offense when using `Other::T.type_alias`" do
     expect_no_offenses(<<~RUBY)
-      good_method
+      Int = Other::T.type_alias {Integer}
     RUBY
   end
 end
