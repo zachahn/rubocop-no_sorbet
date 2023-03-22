e28d35497f3c2c128b0f29ad61f5458356da2ba7
Zach Ahn <engineering@zachahn.com>
2023-03-22 01:09:02 -0400

    Implement NoTypeAssertions

--- lib/rubocop/cop/no_rubocop/no_type_assertions.rb
+++ lib/rubocop/cop/no_rubocop/no_type_assertions.rb
@@ -3,67 +3,55 @@
 module RuboCop
   module Cop
     module NoRubocop
-      # TODO: Write cop description and example of bad / good code. For every
-      # `SupportedStyle` and unique configuration, there needs to be examples.
-      # Examples must have valid Ruby syntax. Do not use upticks.
+      # Do not use type assertions `T.cast`, `T.must`, etc
       #
       # @safety
-      #   Delete this section if the cop is not unsafe (`Safe: false` or
-      #   `SafeAutoCorrect: false`), or use it to explain how the cop is
-      #   unsafe.
+      #   Unsafe
       #
-      # @example EnforcedStyle: bar (default)
-      #   # Description of the `bar` style.
+      # @example
+      #   # bad
+      #   T.let
       #
       #   # bad
-      #   bad_bar_method
+      #   T.cast
       #
       #   # bad
-      #   bad_bar_method(args)
-      #
-      #   # good
-      #   good_bar_method
-      #
-      #   # good
-      #   good_bar_method(args)
-      #
-      # @example EnforcedStyle: foo
-      #   # Description of the `foo` style.
+      #   T.must
       #
       #   # bad
-      #   bad_foo_method
+      #   T.assert_type!
       #
       #   # bad
-      #   bad_foo_method(args)
-      #
-      #   # good
-      #   good_foo_method
-      #
-      #   # good
-      #   good_foo_method(args)
+      #   T.bind
       #
       class NoTypeAssertions < Base
-        # TODO: Implement the cop in here.
-        #
-        # In many cases, you can use a node matcher for matching node pattern.
-        # See https://github.com/rubocop/rubocop-ast/blob/master/lib/rubocop/ast/node_pattern.rb
-        #
-        # For example
-        MSG = "Use `#good_method` instead of `#bad_method`."
+        extend AutoCorrector
 
-        # TODO: Don't call `on_send` unless the method name is in this list
-        # If you don't need `on_send` in the cop you created, remove it.
-        RESTRICT_ON_SEND = %i[bad_method].freeze
+        MSG = "Do not use type assertions `T.cast`, `T.must`, etc"
+        RESTRICT_ON_SEND = %i[let cast must assert_type! bind].freeze
 
-        # @!method bad_method?(node)
-        def_node_matcher :bad_method?, <<~PATTERN
-          (send nil? :bad_method ...)
+        # @!method t_let_cast_assert_type?(node)
+        def_node_matcher :t_let_cast_assert_type?, <<~PATTERN
+          (send (const {nil? | cbase} :T) {:let | :cast | :assert_type!} _ _)
+        PATTERN
+
+        # @!method t_must_bind?(node)
+        def_node_matcher :t_must_bind?, <<~PATTERN
+          (send (const {nil? | cbase} :T) {:must | :bind} _)
         PATTERN
 
         def on_send(node)
-          return unless bad_method?(node)
+          if t_let_cast_assert_type?(node)
+            return add_offense(node) do |corrector|
+              corrector.replace(node, "(#{node.child_nodes[1].source})")
+            end
+          end
 
-          add_offense(node)
+          if t_must_bind?(node)
+            add_offense(node) do |corrector|
+              corrector.replace(node, "(#{node.child_nodes[1].source})")
+            end
+          end
         end
       end
     end
--- spec/rubocop/cop/no_rubocop/no_type_assertions_spec.rb
+++ spec/rubocop/cop/no_rubocop/no_type_assertions_spec.rb
@@ -1,21 +1,60 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::NoRubocop::NoTypeAssertions, :config do
-  let(:config) { RuboCop::Config.new }
+  let(:message) { described_class::MSG }
 
-  # TODO: Write test code
-  #
-  # For example
-  it "registers an offense when using `#bad_method`" do
-    expect_offense(<<~RUBY)
-      bad_method
-      ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
-    RUBY
+  shared_examples "two argument assertion, keep first parameter" do |method_name, type|
+    it "registers an offense when assigning `T.#{method_name}(#{type})` to a variable" do
+      expect_offense(<<~RUBY)
+        foo = T.#{method_name}(some_value, #{type})
+              ^^^^^^^^^^^^^^^^#{"^" * (method_name.size + type.size)} #{message}
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo = (some_value)
+      RUBY
+    end
+
+    it "registers an offense when assigning `::T.#{method_name}(#{type})` to a variable" do
+      expect_offense(<<~RUBY)
+        foo = ::T.#{method_name}(some_value, #{type})
+              ^^^^^^^^^^^^^^^^^^#{"^" * (method_name.size + type.size)} #{message}
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo = (some_value)
+      RUBY
+    end
   end
 
-  it "does not register an offense when using `#good_method`" do
-    expect_no_offenses(<<~RUBY)
-      good_method
-    RUBY
+  shared_examples "one argument assertion, keep parameter" do |method_name, parameter|
+    it "registers an offense when assigning `T.#{method_name}`" do
+      expect_offense(<<~RUBY)
+        foo = T.#{method_name}(#{parameter})
+              ^^^^#{"^" * (method_name.size + parameter.size)} #{message}
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo = (#{parameter})
+      RUBY
+    end
+
+    it "registers an offense when assigning `::T.#{method_name}`" do
+      expect_offense(<<~RUBY)
+        foo = ::T.#{method_name}(#{parameter})
+              ^^^^^^#{"^" * (method_name.size + parameter.size)} #{message}
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo = (#{parameter})
+      RUBY
+    end
   end
+
+  include_examples "two argument assertion, keep first parameter", "let", "T.nilable(Integer)"
+  include_examples "two argument assertion, keep first parameter", "cast", "String"
+  include_examples "two argument assertion, keep first parameter", "assert_type!", "T::Boolean"
+
+  include_examples "one argument assertion, keep parameter", "must", "some_value"
+  include_examples "one argument assertion, keep parameter", "bind", "self"
 end
