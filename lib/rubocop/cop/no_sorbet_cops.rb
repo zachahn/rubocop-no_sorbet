6ce86b2b4918098e2b1938f486949001ed0bc69a
Zach Ahn <engineering@zachahn.com>
2023-03-21 22:08:11 -0400

    Generate more cops

--- lib/rubocop/cop/no_sorbet_cops.rb
+++ lib/rubocop/cop/no_sorbet_cops.rb
@@ -2,3 +2,10 @@
 
 require_relative "no_rubocop/no_extend_t_sig"
 require_relative "no_rubocop/no_sig"
+require_relative "no_rubocop/no_t_absurd"
+require_relative "no_rubocop/no_t_enum"
+require_relative "no_rubocop/no_t_struct"
+require_relative "no_rubocop/no_type_aliases"
+require_relative "no_rubocop/no_type_annotations"
+require_relative "no_rubocop/no_type_assertions"
+require_relative "no_rubocop/no_typed_sigil"
